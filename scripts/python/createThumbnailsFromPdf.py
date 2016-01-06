0aa6e07a0523adc354e68e522e35464cc5c85cf5
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-01-06 12:00:18 -0500

    re-structured files
    - renamed the folders
    - generated icons
    - added grids to pages

---
+++ scripts/python/createThumbnailsFromPdf.py
@@ -0,0 +1,50 @@
+"""
+You need to install wand and ghostscript on your system.
+Check this discussion for step-by-step workflow
+http://stackoverflow.com/questions/13984357/pythonmagick-cant-find-my-pdf-files
+
+The script generates thumbnail images from all pdf pages
+"""
+import os
+from pyPdf import PdfFileReader, PdfFileWriter
+from tempfile import NamedTemporaryFile
+from wand.image import Image
+import json
+
+def pdf2png(pdfFile):
+    reader = PdfFileReader(open(pdfFile, "rb"))
+    path, name = os.path.split(pdfFile)
+    for page_num in xrange(reader.getNumPages()):
+        writer = PdfFileWriter()
+        writer.addPage(reader.getPage(page_num))
+        temp = NamedTemporaryFile(prefix=str(page_num), suffix=".pdf", delete=False)
+        writer.write(temp)
+        temp.close()
+
+        with Image(filename = temp.name, resolution=(300, 300)) as img:
+            ar = 300.0 / img.width
+            img.resize(300, int(img.height * ar))
+            img.compression_quality = 99
+            img.save(filename= os.path.join(path, "thumbnail_%d.png" % (page_num)))
+        os.remove(temp.name)
+
+    del(reader)
+
+
+courseData = r"C:\Users\Administrator\Documents\GitHub\mostapharoudsari.github.io\data\teaching\upenn\arch753\fall15\courseInfo.json"
+studentsFolder = os.path.join(os.path.split(courseData)[0], 'students')
+folders = ["backup", "weather-data-analysis", "meyerson-hall-canopy", "daylighting-I", "daylighting-II", "energy-and-daylighting", "indoor-comfort"]
+
+"""
+with open(courseData, "rb") as cData:
+    data = json.load(cData)
+
+    for assignment, files in student["assignments"].items():
+        for f in files:
+            if f.lower().endswith(".pdf"):
+                pdfFile = os.path.join(baseFolder, assignment, f)
+                pdf2png(pdfFile)
+"""
+
+pdfFile = r"C:\Users\Administrator\Documents\GitHub\mostapharoudsari.github.io\data\teaching\upenn\arch753\fall15\students\jin_jaeho\daylighting-I\Jin_Jaeho_Assignment 1012_Daylighting.pdf"
+pdf2png(pdfFile)
