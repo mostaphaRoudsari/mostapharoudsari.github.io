2eb1bd6085ac37c3a92d52f58762be39fd5f393d
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-11-01 19:09:26 -0400

    Added annual daylight assignment

--- scripts/coursefolder/utilities.py
+++ scripts/coursefolder/utilities.py
@@ -192,11 +192,12 @@ def renameThumbnails(coursefolder, assignments):
 
 
 if __name__ == '__main__':
-    s = r'C:\UPENN\161017'
+    s = r'C:\UPENN\161024\point-in-time-daylight-analysis'
     f = r'C:\Users\Administrator\Documents\GitHub\mostapharoudsari.github.io\data\teaching\upenn\arch753\fall16'
 
     assignments = ("dream-room", "meyerson-hall-canopy", "weather-data-analysis",
-                   "weather-data-analysis-II", "daylighting-I")
-    # dumpFiles('daylighting-I', s, f)
+                   "weather-data-analysis-II", "daylighting-I", 'daylighting-II',
+                   'annual_daylight')
+    # dumpFiles('daylighting-II', s, f)
     # renameThumbnails(f, assignments)
     # updateCourseInformation(f, assignments)
